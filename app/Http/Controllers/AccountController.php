e7661a48504e0216387a48193aee67d1defca150
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-01 07:55:18 -0300

    Add confirm method to account controller for password confirmation

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Helpers\APIResponse;
 use App\Http\Requests\Account\AccountBaseRequest;
+use App\Http\Requests\Account\AccountConfirmRequest;
 use App\Services\Account\IAccountService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +24,12 @@ class AccountController extends Controller
 
         $jsonSuccessReturn = APIResponse::formatJSONPayload('success', $currentUser);
         return response()->json($jsonSuccessReturn, 200);
+    }
+
+    public function confirm(AccountConfirmRequest $request): JsonResponse
+    {
+        $jsonSuccessReturn = APIResponse::formatJSONPayload('success', null);
+        return response()->json($jsonSuccessReturn, 204);
     }
 
     public function store(AccountBaseRequest $request): JsonResponse
