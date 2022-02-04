b45e5143da2df15dcee1d28ec158047bea332b53
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-04 07:13:07 -0300

    Update title page from main blade file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}"/>
-    <title>Larvue</title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <title>xVault | Credentials Manager</title>
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     <style>
         .bg-light {
