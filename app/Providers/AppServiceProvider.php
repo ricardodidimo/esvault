4ea495b8df607996bbf45b4f248daa561e881c9b
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-04 21:19:28 -0300

    Update appServiceProvider boot config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,9 +32,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
-        URL::forceScheme('https');
-        
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         DB::listen(function ($query) {
             Log::debug(
