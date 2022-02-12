ca561a38f9b0ee7b4bef6350e6997c686e419035
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-12 19:29:16 -0300

    update app-logo component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}"/>
-    <title>esVault | Credential Manager</title>
+    <title>esVault | Online Credential Manager</title>
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     <style>
         .bg-light {
