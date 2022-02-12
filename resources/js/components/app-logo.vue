3eda88aabda53e8864385da923db0dae0fe65ffc
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-12 19:29:16 -0300

    update app-logo component

--- resources/js/components/app-logo.vue
+++ resources/js/components/app-logo.vue
@@ -15,16 +15,10 @@
 <template>
   <div>
     <div class="d-flex logo__container">
-      <a href="https://github.com/ricardodidimo">
-        <img class="img-fluid" src="/images/logo__github-icon.svg" alt="author github icon" />
-      </a>
       <div class="d-flex logo__text">
         <small class="fs-4">es</small>
         <h1>Vault</h1>
       </div>
-      <a href="https://www.linkedin.com/in/ricardo-didimo-558630209/">
-        <img class="img-fluid" src="/images/logo__linkedin-icon.svg" alt="author linkedin icon" />
-      </a>
     </div>
   </div>
 </template>
