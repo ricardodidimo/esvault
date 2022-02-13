b9302be2a768524288723a3ef14acf26d8ab590f
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-13 16:24:44 -0300

    Change layout from home page component

--- resources/js/views/home.vue
+++ resources/js/views/home.vue
@@ -6,6 +6,17 @@
   opacity: 0.5;
   text-decoration: underline;
 }
+.blue-background {
+  width: 97vw !important;
+  border-radius: 0rem 0rem 1rem 1rem;
+  padding: 8rem 8rem;
+  transform: skewX(3.5deg);
+  background-color: var(--app-blue);
+  color: white;
+}
+footer > p {
+  opacity: 0.5;
+}
 @media (max-width: 990px) {
   .cards__container {
     flex-direction: column;
@@ -19,54 +30,47 @@
 </style>
 
 <template>
-  <div class="home__container d-flex flex-column justify-content-center align-items-center">
-    <div
+  <div
+    class="
+      home__container
+      d-flex
+      flex-column
+      justify-content-center
+      align-items-center
+    "
+  >
+    <main
       class="
+        blue-background
         d-flex
+        flex-column
         justify-content-center
         align-items-center
-        py-5
-        cards__container
       "
     >
-      <AppCard class="cards__item mx-2" cardTitle="For better protection">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-        consequatur minima est nihil reprehenderit hic modi quis tenetur,
-        laborum mollitia omnis, rem adipisci qui, ad molestiae explicabo eum!
-        Distinctio, vitae.
-      </AppCard>
-      <AppCard
-        class="cards__item mx-2"
-        imageUrl="images/idea-icon.svg"
-        cardTitle="In an easy way"
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-        consequatur minima est nihil reprehenderit hic modi quis tenetur,
-        laborum mollitia omnis, rem adipisci qui, ad molestiae explicabo eum!
-        Distinctio, vitae.
-      </AppCard>
-      <AppCard
-        class="cards__item mx-2"
-        imageUrl="images/lock-icon.svg"
-        cardTitle="In an secure vault"
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-        consequatur minima est nihil reprehenderit hic modi quis tenetur,
-        laborum mollitia omnis, rem adipisci qui, ad molestiae explicabo eum!
-        Distinctio, vitae.
-      </AppCard>
-    </div>
-    <div
-      class="w-100 d-flex flex-column align-items-center justify-content-center"
-    >
-      <router-link :to="{name: 'account-register'}">
-        <AppPrimaryButton
-          class="fs-4"
-        >Get started</AppPrimaryButton>
-      </router-link>
-
-      <router-link :to="{name: 'account-login'}" class="a-link fs-5">Already in</router-link>
-    </div>
+      <h1 class="fs-3 mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </h1>
+      <div>
+        <router-link :to="{ name: 'account-register' }">
+          <AppPrimaryButton class="fs-5">Get started</AppPrimaryButton>
+        </router-link>
+        <router-link :to="{ name: 'account-login' }">
+          <AppPrimaryButton class="fs-5">Already in</AppPrimaryButton>
+        </router-link>
+      </div>
+    </main>
+    <footer class="mt-5 d-flex flex-column align-items-center justify-content-center">
+      <div>
+        <a href="https://github.com/ricardodidimo">
+        <img width="55px" height="55px" src="images/logo__github-icon.svg" alt="github icon">
+        </a>
+        <a href="https://www.linkedin.com/in/ricardo-didimo-558630209">
+        <img width="55px" height="55px" src="images/logo__linkedin-icon.svg" alt="linkedin icon">
+        </a>
+      </div>
+      <p>This application is a demonstration.</p>
+    </footer>
   </div>
 </template>
 
