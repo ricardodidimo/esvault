e55ac7a95f35e28a54b3535ff42a6990ffafc350
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-13 21:04:55 -0300

    Change AppLoading component

--- resources/js/components/app-loading-placeholder.vue
+++ resources/js/components/app-loading-placeholder.vue
@@ -1,32 +1,66 @@
-<style scoped>
-.loading__container{
-  color: var(--app-blue);
-  font-family: var(--app-action-font);
-}
+<!-- 
+  This css loading asset was made by nobuakihonma and 
+  its code can be found at: https://codepen.io/nobuakihonma/pen/dYbqLQ
+-->
 
-.loading__box {
-  width: 30px;
-  height: 30px;
-  animation: example 3s infinite;
-  background-color: var(--app-blue);
-  border-radius: 10%;
+<style scoped>
+.loader-1 {
+	height: 16px;
+	width: 16px;
+	-webkit-animation: loader-1-1 4.8s linear infinite;
+	        animation: loader-1-1 4.8s linear infinite;
 }
-@keyframes example {
-  0% {
-    transform: rotate(20deg);
-  }
-  25% {
-    transform: rotate(45deg);
-  }
-  50% {
-    transform: rotate(90deg);
-  }
-  75% {
-    transform: rotate(-120deg);
-  }
-  100% {
-    transform: rotate(20deg);
-  }
+@-webkit-keyframes loader-1-1 {
+	0%   { -webkit-transform: rotate(0deg); }
+	100% { -webkit-transform: rotate(360deg); }
+}
+@keyframes loader-1-1 {
+	0%   { transform: rotate(0deg); }
+	100% { transform: rotate(360deg); }
+}
+.loader-1 span {
+	display: block;
+	position: absolute;
+	top: 0; left: 0;
+	bottom: 0; right: 0;
+	margin: auto;
+	height: 16px;
+	width: 16px;
+	clip: rect(0, 16px, 16px, 8px);
+	-webkit-animation: loader-1-2 1.2s linear infinite;
+	        animation: loader-1-2 1.2s linear infinite;
+}
+@-webkit-keyframes loader-1-2 {
+	0%   { -webkit-transform: rotate(0deg); }
+	100% { -webkit-transform: rotate(220deg); }
+}
+@keyframes loader-1-2 {
+	0%   { transform: rotate(0deg); }
+	100% { transform: rotate(220deg); }
+}
+.loader-1 span::after {
+	content: "";
+	position: absolute;
+	top: 0; left: 0;
+	bottom: 0; right: 0;
+	margin: auto;
+	height: 16px;
+	width: 16px;
+	clip: rect(0, 16px, 16px, 8px);
+	border: 3px solid var(--app-blue);
+	border-radius: 50%;
+	-webkit-animation: loader-1-3 1.2s cubic-bezier(0.770, 0.000, 0.175, 1.000) infinite;
+	        animation: loader-1-3 1.2s cubic-bezier(0.770, 0.000, 0.175, 1.000) infinite;
+}
+@-webkit-keyframes loader-1-3 {
+	0%   { -webkit-transform: rotate(-140deg); }
+	50%  { -webkit-transform: rotate(-160deg); }
+	100% { -webkit-transform: rotate(140deg); }
+}
+@keyframes loader-1-3 {
+	0%   { transform: rotate(-140deg); }
+	50%  { transform: rotate(-160deg); }
+	100% { transform: rotate(140deg); }
 }
 </style>
 
@@ -35,8 +69,7 @@
     <div
       class="loading__container d-flex justify-content-center align-items-center"
     >
-      <div class="loading__box me-3"></div>
-      <p class="px-3">Loading...</p>
+      <div class="loader-1 center"><span></span></div>
     </div>
   </div>
 </template>
