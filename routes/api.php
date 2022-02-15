2d8335c451093f7c1f125ce9f93281f3e2916f49
ricardo didimo <ricardodidimodev@outlook.com>
2022-02-14 23:24:40 -0300

    Remove routes from API routes file

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
-use App\Http\Controllers\CredentialController;
-use App\Http\Controllers\EmailVerifyController;
 use App\Http\Controllers\UserController;
-use App\Http\Middleware\CustomEnsureEmailVerified;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,55 +45,4 @@ Route::middleware('auth:sanctum')->group(function () {
         '/account',
         [AccountController::class, 'destroy']
     )->name('account.destroy');
-
-    Route::get(
-        '/email/is-verified',
-        [EmailVerifyController::class, 'isVerified']
-    )->name('verification.checkIfVerified');
-
-    Route::post(
-        '/email/verification-notification',
-        [EmailVerifyController::class, 'resendEmail']
-    )->name('verification.send');
-});
-
-Route::middleware(['auth:sanctum', 'signed'])->group(function () {
-    Route::get(
-        '/email/verify/{id}/{hash}',
-        [EmailVerifyController::class, 'verifyEmail']
-    )->name('verification.verify');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    CustomEnsureEmailVerified::class
-])->group(function () {
-    Route::get('/isAuth', function (Request $request) {
-        return response("Hello {$request->user()->name}. You are authenticated.", 200);
-    });
-
-    Route::get(
-        '/credentials/{id}',
-        [CredentialController::class, 'show']
-    )->name('credentials.show')->whereNumber('id');
-
-    Route::get(
-        '/credentials/{title?}',
-        [CredentialController::class, 'index']
-    )->name('credentials.index');
-
-    Route::post(
-        '/credentials',
-        [CredentialController::class, 'store']
-    )->name('credentials.create');
-
-    Route::put(
-        '/credentials/{id}',
-        [CredentialController::class, 'update']
-    )->name('credentials.update');
-
-    Route::delete(
-        '/credentials/{id}',
-        [CredentialController::class, 'destroy']
-    )->name('credentials.destroy');
 });
